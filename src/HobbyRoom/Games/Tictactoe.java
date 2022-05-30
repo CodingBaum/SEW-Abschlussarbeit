a2f8d9918c908bda07b6aae83ff19143cb7d17f5
Christoph A <9116@htl.rennweg.at>
2022-05-30 10:36:40 +0200

    TicTacToe Feld v1
    Im Momnt noch ohne  Bilder

--- src/HobbyRoom/Games/Tictactoe.java
+++ src/HobbyRoom/Games/Tictactoe.java
@@ -6,23 +6,78 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 import static HobbyRoom.Client.setPos;
 
 public class Tictactoe {
+    private static TextArea output;
 
     public static Stage createStage(Client client) {
         Stage stage = new Stage();
         stage.setResizable(false);
         stage.setTitle("Tictactoe");
 
-        GridPane grid = new GridPane();
 
-        Scene scene = new Scene(grid, 600, 600);
+        AnchorPane grid = new AnchorPane();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double height = 350;
+        double width = 350;
+        int buttonheight = 100;
+        int buttonwidth = 100;
+
+
+        Button A1 = new Button();
+        A1.setPrefHeight(buttonheight);
+        A1.setPrefWidth(buttonwidth);
+        setPos(A1, 10, 10);
+        Button A2 = new Button();
+        A2.setPrefHeight(buttonheight);
+        A2.setPrefWidth(buttonwidth);
+        setPos(A2, buttonwidth + 15, 10);
+        Button A3 = new Button();
+        A3.setPrefHeight(buttonheight);
+        A3.setPrefWidth(buttonwidth);
+        setPos(A3, buttonwidth*2 + 20, 10);
+        Button B1 = new Button();
+        B1.setPrefHeight(buttonheight);
+        B1.setPrefWidth(buttonwidth);
+        setPos(B1, 10, buttonheight + 15);
+        Button B2 = new Button();
+        B2.setPrefHeight(buttonheight);
+        B2.setPrefWidth(buttonwidth);
+        setPos(B2, buttonwidth + 15, buttonheight + 15);
+        Button B3 = new Button();
+        B3.setPrefHeight(buttonheight);
+        B3.setPrefWidth(buttonwidth);
+        setPos(B3, buttonwidth*2 + 20, buttonheight + 15);
+        Button C1 = new Button();
+        C1.setPrefHeight(buttonheight);
+        C1.setPrefWidth(buttonwidth);
+        setPos(C1, 10, buttonheight*2 + 20);
+        Button C2 = new Button();
+        C2.setPrefHeight(buttonheight);
+        C2.setPrefWidth(buttonwidth);
+        setPos(C2, buttonwidth + 15, buttonheight*2 + 20);
+        Button C3 = new Button();
+        C3.setPrefHeight(buttonheight);
+        C3.setPrefWidth(buttonwidth);
+        setPos(C3, buttonwidth*2 + 20, buttonheight*2 + 20);
+        output = new TextArea();
+        output.setWrapText(true);
+        output.setPrefHeight(1);
+        output.setPrefWidth(width-10);
+        output.setText("Spieler 1 am Zug");
+        setPos(output, 10, height-30);
+
+        grid.getChildren().addAll(A1, A2, A3, B1, B2, B3, C1, C2, C3, output);
+        Scene scene = new Scene(grid, width, height);
         stage.setScene(scene);
 
         return stage;
